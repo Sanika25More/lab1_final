989b82e858abbc5761049a3519f3c32d53583558
Sanika25More <moresanika2503@gmail.com>
2025-07-25 20:04:31 +0530

    Initial Flask project added

---
+++ app/app.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Hello from Flask!'
+
+@app.route('/api')
+def api():
+    return jsonify({"message": "Initial API response"})
+
+if __name__ == '__main__':
+    app.run(debug=True)
